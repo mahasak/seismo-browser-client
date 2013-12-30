ae82e9e88f0053c55811667a84252239fcaa8266
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-30 14:12:31 +0200

    grunt init

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		meta: {
+			version: '0.0.1'
+		},
+
+		jshint: {
+			options: {
+				"asi" : false,
+				"bitwise" : true,
+				"boss" : false,
+				"curly" : true,
+				"debug": false,
+				"devel": false,
+				"eqeqeq": true,
+				"evil": false,
+				"expr": true,
+				"forin": false,
+				"immed": true,
+				"latedef" : false,
+				"laxbreak": false,
+				"multistr": true,
+				"newcap": true,
+				"noarg": true,
+				"node" : true,
+				"browser": true,
+				"noempty": false,
+				"nonew": true,
+				"onevar": false,
+				"plusplus": false,
+				"regexp": false,
+				"strict": false,
+				"sub": false,
+				"trailing" : true,
+				"undef": true,
+				"unused": "vars",
+				globals: {
+					angular: true,
+					require: true,
+					define: true
+				}
+			},
+			js: ['src/**/*.js']
+		}
+	});
+
+	// Laoded tasks
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint']);
+};
